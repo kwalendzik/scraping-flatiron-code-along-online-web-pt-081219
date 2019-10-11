dd8cdedc9fcef69bd440155b0e8c195122378d5a
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-10-11 04:22:15 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,19 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
+  
+  def get_page
+  end
+  
+  #def get_courses
+  #end
+  
+  #def make_courses
+  #end
   
   def print_courses
     self.make_courses
@@ -17,6 +27,3 @@ class Scraper
   end
   
 end
-
-
-
